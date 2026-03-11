714837caaa18ebc47c883ddbf15d2f568d54fe75
Eric Sengmany <eric.k.sengmany.mil@army.mil>
2026-03-11 13:04:56 -0500

    Day 11 push

---
+++ HTML/Object/script.js
@@ -0,0 +1,24 @@
+//ccool stuff about objects
+
+let dog = 'woof'
+let cat = 'meow'
+
+let animals = {
+  dog,
+  cat
+}
+
+console.log(animals.cat)
+
+//two referene values in objects
+//bracket notation and dot notation
+//animals[`dog`]
+//animals.dog
+
+console.log(animals[`dog`])
+
+console.log(animals.dog)
+// This is the value of dog
+
+console.log(animals[dog])
+//This is looking for animals with the value of dog (woof)
